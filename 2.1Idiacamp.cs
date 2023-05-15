54811a22f613511fb9da5149dee735d74e8a0480
nnxtip <123537484+nnxtip@users.noreply.github.com>
2023-05-15 15:31:19 +0700

    Add files via upload

---
+++ 2.1Idiacamp.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the maximum capacity of the water tank: ");
+        double Vmax = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the average volume of water consumed during breaks: ");
+        double Vdrink = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the volume of water refilled during each refill: ");
+        double Vfill = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the time interval between breaks: ");
+        int Tday = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the time interval between refills: ");
+        int Tfill = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter the total duration of the activity in hours: ");
+        int Ttotal = Convert.ToInt32(Console.ReadLine());
+
+        double Vremaining = Vmax;
+        bool enoughWater = true;
+        bool overflowWater = false;
+
+        for (int t = 0; t < Ttotal; t++)
+        {
+            if (t % Tday == 0)
+            {
+                Vremaining -= Vdrink;
+                if (Vremaining < 0)
+                {
+                    enoughWater = false;
+                    break;
+                }
+            }
+
+            if (t % Tfill == 0)
+            {
+                Vremaining += Vfill;
+                if (Vremaining > Vmax)
+                {
+                    overflowWater = true;
+                    break;
+                }
+            }
+        }
+
+        if (enoughWater)
+        {
+            Console.WriteLine("Enough Water, {0} left", Vremaining);
+            if (overflowWater)
+            {
+                Console.WriteLine("Overflow Water");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Not Enough Water");
+        }
+    }
+}
---
+++ 2.2Idiacamp.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double B1, B2, B3, payment;
+        
+        Console.Write("Enter balance 1: ");
+        B1 = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter balance 2: ");
+        B2 = double.Parse(Console.ReadLine());
+
+        Console.Write("Enter balance 3: ");
+        B3 = double.Parse(Console.ReadLine());
+
+        while (true)
+        {
+            Console.Write("Enter payment amount (0 or negative number to stop): ");
+            payment = double.Parse(Console.ReadLine());
+
+            if (payment <= 0)
+                break;
+
+            if (B1 >= payment)
+                B1 -= payment;
+            else if (B2 >= payment)
+                B2 -= payment;
+            else if (B3 >= payment)
+                B3 -= payment;
+        }
+
+        Console.WriteLine($"Balance 1: {B1}, Balance 2: {B2}, Balance 3: {B3}");
+
+        if (B1 + B2 + B3 > 0)
+            Console.WriteLine("Left: " + (B1 + B2 + B3));
+    }
+}
